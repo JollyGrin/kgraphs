ad7b312339d79e6c0cf31e79d28e84a8dd4565be
jollygrin <jollygrin@protonmail.com>
2019-06-28 13:10:13 +0200

    moved search function into its own model

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,5 @@
-// import Search from './models/Search';
+import Search from './models/Search';
 import { elements, renderLoader, clearLoader } from './views/base';
 
-// const search = new Search('pizza');
-// console.log(search);
-
-import axios from 'axios';
-
-async function getResults(query) {
-    // const proxy = 'https://cors-anywhere.herokuapp.com/';
-    // const key = '9c407b7c371776692fb3786445ef8801';
-    // try {
-    //     const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-    //     this.result = res.data.recipes;
-    //     console.log(this.result);
-    // } catch (error) {
-    //     console.log(error)
-    // }
-
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '9c407b7c371776692fb3786445ef8801';
-    try {
-        const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        console.log(error);
-    }
-    
-}
-
-
-getResults('pasta');
-
-// async function getResults(query) {
-//     axios('http://www.food2fork.com/api/search');
-// }
+const search = new Search('pizza');
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,17 +5,31 @@ export default class Search {
         this.query = query;
     }
 
-    async getResults(query) {
+    async getResults() {
+
+        // Notifies via console if the API limit was reached
+        const apiLimitCatch = function (result) {
+            const resultD = result.data.error;
+            
+            if (resultD === 'limit') {
+                console.log('API error limit reached. Make a new account at food2fork.com and input the new api key');
+            }
+        };
+    
         const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = '9c407b7c371776692fb3786445ef8801';
+        const key = '259a740ab0267df343cd20b4177a3d16';
+        // const key = '9c407b7c371776692fb3786445ef8801';
+
         try {
             const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
+            apiLimitCatch(res); // will consolelog if daily api limit of food2fork is reached
             console.log(this.result);
-        } catch (error) {
-            alert(error);
+        } catch(error) {
+            console.log('We got an error, chief');
+            console.log(error);  
         }
+        
     }
+    
 }
-
-// getResults('pasta');
