08aa7de0a273b9c370ead01b464d3341c5907f35
jollygrin <jollygrin@protonmail.com>
2019-06-28 14:07:41 +0200

    search pizza when you click search. Array in console

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
-import { elements, renderLoader, clearLoader } from './views/base';
 
-const search = new Search('pizza');
-search.getResults();
+/** Global state of the app
+ * Search object
+ * Current recipe object
+ * Shopping list object
+ * Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // get query from view
+    const query = 'pizza';
+
+    if (query) {
+        // new search object & add to state
+        state.search = new Search(query);
+    }
+
+    // prepare UI for results
+
+    // search for recipes
+    await state.search.getResults(query);
+
+    // render results to ui
+    console.log(state.search.result);
+};
+
+document.querySelector('.search__div').addEventListener('click', e => {
+    controlSearch();
+    console.log('test: search button clicked');
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -17,14 +17,15 @@ export default class Search {
         };
     
         const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = '259a740ab0267df343cd20b4177a3d16';
+        // const key = '259a740ab0267df343cd20b4177a3d16';
         // const key = '9c407b7c371776692fb3786445ef8801';
+        const key = '6fc289cb24c4596f929ebbd12997fe33';
 
         try {
             const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
             apiLimitCatch(res); // will consolelog if daily api limit of food2fork is reached
-            console.log(this.result);
+            // console.log(this.result);
         } catch(error) {
             console.log('We got an error, chief');
             console.log(error);  
