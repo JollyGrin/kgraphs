e914a139b81a8693ba8e4c55684f8c2d418ef9e3
jollygrin <jollygrin@protonmail.com>
2019-06-28 11:41:19 +0200

    troubleshooting api request. Using food2fork api as a test. Successfully loads 30 objects within an array.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,37 @@
+// import Search from './models/Search';
 import { elements, renderLoader, clearLoader } from './views/base';
 
-// export const elements = {
-//     searchInput: document.querySelector('.search__field')
-// };
+// const search = new Search('pizza');
+// console.log(search);
+
+import axios from 'axios';
+
+async function getResults(query) {
+    // const proxy = 'https://cors-anywhere.herokuapp.com/';
+    // const key = '9c407b7c371776692fb3786445ef8801';
+    // try {
+    //     const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+    //     this.result = res.data.recipes;
+    //     console.log(this.result);
+    // } catch (error) {
+    //     console.log(error)
+    // }
+
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '9c407b7c371776692fb3786445ef8801';
+    try {
+        const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        console.log(error);
+    }
+    
+}
+
+
+getResults('pasta');
+
+// async function getResults(query) {
+//     axios('http://www.food2fork.com/api/search');
+// }
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '9c407b7c371776692fb3786445ef8801';
+        try {
+            const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
+// getResults('pasta');
